71ad1333b01151e205d315b2d36beff45c57f1e1
Ericka Tuchan <125705781+ErickaT23@users.noreply.github.com>
2026-09-24 01:16:14 -0600

    first commit

---
+++ config.js
@@ -0,0 +1,66 @@
+const config = {
+    admin: {
+        adminKey: "twodesign123",
+        keyParam: "key",
+        legacyKeyParam: "admin"
+    },
+
+    seo: {
+        titulo: "Tony Lopez",
+        descripcion: "Celebremos juntos la vida",
+        autor: "Two Design"
+    },
+
+    pareja: {
+        nombres: "Tony Lopez",
+        fecha: "24-10-2026",
+        fechaVisible: "24.10.2026"
+    },
+
+    musica: {
+        titulo: "Nuestra Canción",
+        archivo: "audio/cancion.mp3"
+    },
+
+    event: {
+        defaultEventId: "tonylopez2026",
+        eventIdParam: "eventId",
+        eventDate: "2026-10-24T16:00:00",
+        eventName: "Tony Lopez",
+        maxGuests: 6
+    },
+
+    evento: {
+        eventDate: "2026-10-24T16:00:00",
+        bendicion: {
+            titulo: "Celebración",
+            lugar: "Tony's Home",
+            hora: "4:00 PM",
+            direccion: "3029 Ambersweet Pl, Clermont, FL 34711, United States",
+            ubicacionUrl: "https://maps.app.goo.gl/omoCm7fXArRfcXnv6"
+        },
+        recepcion: {
+            titulo: "Celebración",
+            lugar: "Tony's Home",
+            hora: "4:00 PM",
+            direccion: "3029 Ambersweet Pl, Clermont, FL 34711, United States",
+            ubicacionUrl: "https://maps.app.goo.gl/omoCm7fXArRfcXnv6"
+        }
+    },
+
+    textos: {
+        mensajeInvitado: "Qué alegría celebrar contigo",
+        mensajePases: "Hemos reservado para ti {pases}"
+    },
+
+    footer: {
+        hashtag: "#TonyLopez27",
+        instagramUrl: "https://instagram.com/thetwodesign",
+        facebookUrl: "https://facebook.com/thetwodesign",
+        marcaTexto: "Diseño",
+        marcaNombre: "Two Design",
+        marcaUrl: "https://twodesign.com"
+    }
+};
+
+window.config = config;
